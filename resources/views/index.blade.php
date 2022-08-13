86e2210d2037082edd79ddd3c5fe72ec31325409
hesyboy <hesamahmoodi@gmail.com>
2022-08-13 23:05:48 +0430

    emrooz

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
     <script src="{{asset('js/app.js')}}"></script>
     <link
   rel="stylesheet"
@@ -14,8 +15,15 @@
 <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
     <title>Document</title>
 </head>
-<body>
+<body style="background-color: #000000; animation:example 80s infinite;
+background-image: url('{{asset('images/back.png')}}'); ";>
 
+<style>
+    @keyframes example {
+  from {background-position-x: 0px;}
+  to {background-position-x: 1000px;}
+}
+</style>
     <header class="sticky top-0 z-50">
         <div class="navbar bg-black py-5 px-5">
             <div class="navbar-start">
@@ -79,97 +87,92 @@
           <div class="h-1 bg-zinc-900"></div>
     </header>
 
-    <div class="bg-zinc-900 " >
+    <div class=" " >
 
         {{-- level1
              --}}
 
-             <div class=" bg-zinc-900 py-10" style="background-color: #000000;
-             background-image: url('https://www.transparenttextures.com/patterns/stardust.png'); background-attachment: fixed;";>
-                <div class="container mx-auto">
-                    <div class="flex flex-col gap-8 items-center justify-center py-10">
+             <div class="  h-[90vh] py-10" >
+                <div class="container mx-auto h-full">
+                    <div class="flex flex-col gap-8  justify-between h-full ">
+
+                        <div class="flex flex-col gap-8 items-center justify-between group">
+                            <div class="bg-yellow-600 p-3 rounded-full">
+                                <div class="swiper mySwiper1 bg-black rounded-full p-3" style="
+                                width: 300px;
+                                height: 300px;
+                                ">
+                                    <div class="swiper-wrapper">
+                                      <div class="swiper-slide">
+                                        <img src="{{asset('/images/slide1.png')}}" class="w-[300px]" />
+                                      </div>
+                                      <div class="swiper-slide">
+                                        <img src="{{asset('/images/slide2.png')}}" class="w-[300px]" />
+                                      </div>
+                                      <div class="swiper-slide">
+                                        <img src="{{asset('/images/slide3.png')}}" class="w-[300px]" />
+                                      </div>
+                                      <div class="swiper-slide">
+                                        <img src="{{asset('/images/slide4.png')}}" class="w-[300px]" />
+                                      </div>
+                                    </div>
+
+                                  </div>
+
+                                  <!-- Swiper JS -->
+                                  <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+
+                                  <!-- Initialize Swiper -->
+                                  <script>
+                                    var swiper = new Swiper(".mySwiper1", {
+                                      effect: "flip",
+                                      grabCursor: true,
+                                      autoplay: {
+                                        delay: 1500,
+                                        disableOnInteraction: false,
+                                        },
+
+                                    });
+                                  </script>
+                                {{-- <img src="{{asset('/images/slide1.png')}}" class="w-[300px]" /> --}}
+                            </div>
 
 
-                        <div>
-                            <div class="swiper mySwiper1" style="
-                            width: 300px;
-                            height: 300px;
-                        ">
-                                <div class="swiper-wrapper">
-                                  <div class="swiper-slide">
-                                    <img src="{{asset('/images/slide1.png')}}" class="w-[300px]" />
-                                  </div>
-                                  <div class="swiper-slide">
-                                    <img src="{{asset('/images/slide1.png')}}" class="w-[300px]" />
-                                  </div>
-                                  <div class="swiper-slide">
-                                    <img src="{{asset('/images/slide1.png')}}" class="w-[300px]" />
-                                  </div>
-                                  <div class="swiper-slide">
-                                    <img src="{{asset('/images/slide1.png')}}" class="w-[300px]" />
-                                  </div>
-                                  <div class="swiper-slide">
-                                    <img src="{{asset('/images/slide1.png')}}" class="w-[300px]" />
-                                  </div>
-                                  <div class="swiper-slide">
-                                    <img src="{{asset('/images/slide1.png')}}" class="w-[300px]" />
-                                  </div>
-                                </div>
 
-                              </div>
 
-                              <!-- Swiper JS -->
-                              <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
-
-                              <!-- Initialize Swiper -->
-                              <script>
-                                var swiper = new Swiper(".mySwiper1", {
-                                  effect: "flip",
-                                  grabCursor: true,
-                                  autoplay: {
-                                    delay: 1500,
-                                    disableOnInteraction: false,
-                                    },
-
-                                });
-                              </script>
-                            {{-- <img src="{{asset('/images/slide1.png')}}" class="w-[300px]" /> --}}
+                            <div class="flex flex-col gap-1 items-center justify-center">
+                                <h1 class="text-7xl text-zinc-300 tracking-[6px]">AMUNRA NFT</h1>
+                                <p class="text-5xl text-yellow-600 font1 tracking-[4px]">Play To Earn</p>
+                            </div>
                         </div>
 
-
-
-                        <div class="flex flex-col gap-5 items-center justify-center">
-                            <h1 class="text-7xl text-gray-100 font1 tracking-[6px]">AMUNRA NFT</h1>
-                            <p class="text-4xl text-gray-100 font1 tracking-[4px]">Play To Earn</p>
-                        </div>
                         <div class="flex flex-col gap-5 justify-center items-center">
-                            <div class=" text-xl text-gray-100 mt-14">Follow Us</div>
+                            <div class=" text-xl text-gray-400 mt-14">Follow Us</div>
                             <div class="flex gap-8">
-                                <a class="flex flex-col justify-center items-center text-gray-100 hover:text-yellow-600
+                                <a class="flex flex-col justify-center items-center text-gray-300 hover:text-yellow-600
                                 transition-all duration-300 hover:scale-95 " href="">
                                     <ion-icon name="logo-instagram" class="text-5xl "></ion-icon>
                                     <span class="">Instagram</span>
                                 </a>
-                                <a class="flex flex-col justify-center items-center" href="">
-                                    <ion-icon name="logo-discord" class="text-5xl text-gray-100"></ion-icon>
-                                    <span class="text-gray-100">Discord</span>
+                                <a class="flex flex-col justify-center items-center text-gray-300 hover:text-yellow-600
+                                transition-all duration-300 hover:scale-95 " href="">
+                                    <ion-icon name="logo-discord" class="text-5xl "></ion-icon>
+                                    <span class="">Discord</span>
                                 </a>
-                                <a  class="flex flex-col justify-center items-center" href="">
-                                    <ion-icon name="logo-twitter" class="text-5xl text-gray-100"></ion-icon>
-                                    <span class="text-gray-100">Twitter</span>
+                                <a  class="flex flex-col justify-center items-center text-gray-300 hover:text-yellow-600
+                                transition-all duration-300 hover:scale-95 " href="">
+                                    <ion-icon name="logo-twitter" class="text-5xl "></ion-icon>
+                                    <span class="">Twitter</span>
                                 </a>
-                                <a class="flex flex-col justify-center items-center " href="">
-                                    <ion-icon name="navigate-circle" class="text-5xl text-gray-100"></ion-icon>
-                                    <span class="text-gray-100">Telegram</span>
+                                <a class="flex flex-col justify-center items-center text-gray-300 hover:text-yellow-600
+                                transition-all duration-300 hover:scale-95 " href="">
+                                    <ion-icon name="navigate-circle" class="text-5xl "></ion-icon>
+                                    <span class="">Telegram</span>
                                 </a>
                             </div>
                         </div>
 
-                        <div>
-                            <div class=" flex flex-col items-center justify-center p-1">
-                                <ion-icon name="caret-down" class="text-4xl text-yellow-600 mt-1"></ion-icon>
-                            </div>
-                        </div>
+
 
                     </div>
                 </div>
@@ -177,12 +180,93 @@
 
              </div>
 
-        {{-- level2 --}}
-        <div class="flex flex-col bg-zinc-900  relative">
+
+                     {{-- level2 --}}
+        <div class="flex flex-col   relative my-40">
             <div class="container mx-auto ">
 
-                <div class="grid grid-cols-2 gap-10 items-center">
-                    <div class="p-5 flex items-center justify-center">
+                <div class="">
+
+                    <div class="flex flex-col gap-7 items-center">
+                        <div class="flex flex-col items-center gap-8">
+                            <div class="flex flex-col items-center ">
+                                <img src="{{asset('/images/logo2.png')}}" class="w-[300px]" />
+                                <h1 class="text-7xl text-gray-300 font1 tracking-[6px]"> World Of Amunra</h1>
+                            </div>
+
+                            <p class="text-xl text-gray-400 tracking-[3px] leading-10 text-center">Amunra is old-school fighting game in metaverse and it has 12 master characters
+                                 of fighters . Project aiming to restore the amunra civilization in the metaverse and to protect cultural heritage
+                                  and environments all around the world. First of all we are working with some  absolutely intelligence artists to
+                                  bring you stunning 2D and 3D collection. Our upcoming 2D collection consists of another eleven 10000 nfts generated
+                                   from 650 all hand drawn unique traits . Our 3D collection will come before gaming launch .we believe that our game
+                                    is world class and so attractive and interesting. The teams project consists of our 3 founder , our 7 artists ,
+                                    5 nft developers , 2 gaming developers and 3 moderators.</p>
+                            <div>
+                                <span class="countdown font-mono text-6xl">
+                                    <span style="--value:57;"></span>
+                                  </span>
+                            </div>
+                            <p class="text-xl text-gray-400">EACH AMUNRA NFT HAS A UNIQUE SET OF TRAITS AND UNLOCKS VARYING UNIQUE LEVELS OF ACCESS AND PERKS FOR ITS OWNER</p>
+                        </div>
+                        <div class="flex justify-center gap-5 w-full">
+                            <a class="bg-gradient-to-r from-yellow-500 to-orange-600
+                            transition-all duration-300 hover:scale-95
+                             text-gray-100 p-3 rounded-lg flex justify-center items-center gap-2">
+                                <ion-icon name="bag-handle" class="text-4xl "></ion-icon>
+                                <span class="text-xl">Mint NFT</span>
+                            </a>
+                            <a class="bg-gradient-to-r from-indigo-500 to-indigo-800
+                            transition-all duration-300 hover:scale-95
+                             text-gray-100 p-3 rounded-lg flex justify-center items-center gap-2">
+                                <ion-icon name="wallet" class="text-4xl "></ion-icon>
+                                <span class="text-xl">Connect Wallet</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <img src="{{asset('images/wall2.png')}}" alt="" srcset="">
+            </div>
+
+            {{-- <div class="relative bottom-20 z-0">
+                <svg width="100%" height="100%" id="svg" viewBox="0 0 1440 400" xmlns="http://www.w3.org/2000/svg" class="transition duration-300 ease-in-out delay-150"><style>
+                    .path-0{
+                      animation:pathAnim-0 4s;
+                      animation-timing-function: linear;
+                      animation-iteration-count: infinite;
+                    }
+                    @keyframes pathAnim-0{
+                      0%{
+                        d: path("M 0,400 C 0,400 0,200 0,200 C 211.86666666666667,222.13333333333333 423.73333333333335,244.26666666666668 561,248 C 698.2666666666667,251.73333333333332 760.9333333333334,237.06666666666666 895,226 C 1029.0666666666666,214.93333333333334 1234.5333333333333,207.46666666666667 1440,200 C 1440,200 1440,400 1440,400 Z");
+                      }
+                      25%{
+                        d: path("M 0,400 C 0,400 0,200 0,200 C 151.46666666666664,181.33333333333331 302.9333333333333,162.66666666666666 470,153 C 637.0666666666667,143.33333333333334 819.7333333333333,142.66666666666669 984,152 C 1148.2666666666667,161.33333333333331 1294.1333333333332,180.66666666666666 1440,200 C 1440,200 1440,400 1440,400 Z");
+                      }
+                      50%{
+                        d: path("M 0,400 C 0,400 0,200 0,200 C 119.73333333333335,180.8 239.4666666666667,161.6 412,158 C 584.5333333333333,154.4 809.8666666666666,166.4 990,176 C 1170.1333333333334,185.6 1305.0666666666666,192.8 1440,200 C 1440,200 1440,400 1440,400 Z");
+                      }
+                      75%{
+                        d: path("M 0,400 C 0,400 0,200 0,200 C 190.2666666666667,171.06666666666666 380.5333333333334,142.13333333333333 557,135 C 733.4666666666666,127.86666666666667 896.1333333333334,142.53333333333336 1041,157 C 1185.8666666666666,171.46666666666664 1312.9333333333334,185.73333333333332 1440,200 C 1440,200 1440,400 1440,400 Z");
+                      }
+                      100%{
+                        d: path("M 0,400 C 0,400 0,200 0,200 C 211.86666666666667,222.13333333333333 423.73333333333335,244.26666666666668 561,248 C 698.2666666666667,251.73333333333332 760.9333333333334,237.06666666666666 895,226 C 1029.0666666666666,214.93333333333334 1234.5333333333333,207.46666666666667 1440,200 C 1440,200 1440,400 1440,400 Z");
+                      }
+                    }</style><path d="M 0,400 C 0,400 0,200 0,200 C 211.86666666666667,222.13333333333333 423.73333333333335,244.26666666666668 561,248 C 698.2666666666667,251.73333333333332 760.9333333333334,237.06666666666666 895,226 C 1029.0666666666666,214.93333333333334 1234.5333333333333,207.46666666666667 1440,200 C 1440,200 1440,400 1440,400 Z" stroke="none" stroke-width="0" fill="#18181bff" class="transition-all duration-300 ease-in-out delay-150 path-0"></path></svg>
+            </div> --}}
+        </div>
+
+
+
+
+
+        {{-- level2 --}}
+        <div class="flex flex-col   relative my-40">
+            <div class="container mx-auto ">
+
+                <div class="">
+                    {{-- <div class="p-5 flex items-center justify-center">
                         <div class="col-span-4">
                             <style>
 
@@ -202,7 +286,7 @@
                                 }
 
                               </style>
-                            <div class="swiper myswiper" style="width: 400px; height: 550px;">
+                            <div class="swiper myswiper" style="width: 400px; height: 600px;">
                                 <div class="swiper-wrapper">
                                   <div class="swiper-slide " style="background-color: transparent">
                                     <img src="{{asset('/images/amunra1.png')}}" class="w-full" />
@@ -224,14 +308,23 @@
                                 });
                               </script>
                         </div>
-                    </div>
+                    </div> --}}
                     <div class="flex flex-col gap-7 items-center">
-                        <div class="flex flex-col gap-7">
-                            <h1 class="text-7xl font-bold text-gray-100">Amunra NFT</h1>
+                        <div class="flex flex-col items-center gap-1">
+                            <div class="flex flex-col items-center ">
+                                <img src="{{asset('/images/logo1.png')}}" class="w-[200px]" />
+                                <h1 class="text-7xl text-gray-100 font1 tracking-[6px]">Amunra NFT</h1>
+                            </div>
+
                             <p class="text-4xl text-gray-100">Play To Earn</p>
+                            <div>
+                                <span class="countdown font-mono text-6xl">
+                                    <span style="--value:57;"></span>
+                                  </span>
+                            </div>
                             <p class="text-xl text-gray-400">EACH AMUNRA NFT HAS A UNIQUE SET OF TRAITS AND UNLOCKS VARYING UNIQUE LEVELS OF ACCESS AND PERKS FOR ITS OWNER</p>
                         </div>
-                        <div class="flex gap-5 w-full">
+                        <div class="flex justify-center gap-5 w-full">
                             <a class="bg-gradient-to-r from-yellow-500 to-orange-600
                             transition-all duration-300 hover:scale-95
                              text-gray-100 p-3 rounded-lg flex justify-center items-center gap-2">
